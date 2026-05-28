9618d7414961f691b244503f37d20303eff77ae6
Eric Salazar Moreira <esalazar@private.convista.de>
2026-05-28 10:21:42 +0000

    tarea 2 formatter

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "dev2026/model/formatter"
+], (Controller, formatter) => {
     "use strict";
 
     return Controller.extend("dev2026.controller.Main", {
+        formatter: formatter, 
+
         onInit() {
         }
     });
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -3,19 +3,20 @@ sap.ui.define([], () => {
     return {
         formatStatusIcon(sStatus) {
             switch (sStatus) {
-                case "1": return "sap-icon://message-error";    // Negativo
-                case "2": return "sap-icon://message-warning";  // Critico
-                case "3": return "sap-icon://message-success";  // Positivo
+                case 1 : return "sap-icon://message-error";    // Negativo
+                case 2: return "sap-icon://message-warning";  // Critico
+                case 3: return "sap-icon://message-success";  // Positivo
                 default:  return "sap-icon://question-mark";    
             }
         },
         formatStatusColor(sStatus) {
             switch (sStatus) {
-                case "1": return "Negative";    // Negativo
-                case "2": return "Critical";    // Critico
-                case "3": return "Positive";    // Positivo
-                default:  return "Neutral";   
+                case 1: return "#ff000d";  // rojo
+                case 2: return "#eee60d";  // amarillo
+                case 3: return "#00ff3c";  // verde
+                default:  return "#dddddd";   // 0 o vacio
             }
         }
+        
     };
 });
