8fb6b9d035583dff9cddec7f3266331f67e94dae
Eric Salazar Moreira <esalazar@private.convista.de>
2026-05-28 12:34:07 +0000

    tarea5

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,13 +1,41 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "dev2026/model/formatter"
-], (Controller, formatter) => {
+    "dev2026/model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, formatter, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("dev2026.controller.Main", {
-        formatter: formatter, 
+        formatter: formatter,
 
         onInit() {
+        },
+
+        onBuscar(oEvent) {
+            const sQuery = oEvent.getParameter("query") || oEvent.getParameter("newValue") || "";
+            const oBinding = this.byId("currencyTable").getBinding("items");
+            const aFilters = [];
+            if (sQuery) {
+                aFilters.push(new Filter({
+                    filters: [
+                        new Filter("Name", FilterOperator.Contains, sQuery),
+                        new Filter("Region", FilterOperator.Contains, sQuery)
+                    ],
+                    and: false
+                }));
+            }
+            oBinding.filter(aFilters);
+        },
+
+        onFiltrarPorMoneda(oEvent) {
+            const sClave = oEvent.getSource().getSelectedKey();
+            const oBinding = this.byId("currencyTable").getBinding("items");
+            const aFilters = [];
+            if (sClave) {
+                aFilters.push(new Filter("Waers", FilterOperator.EQ, sClave));
+            }
+            oBinding.filter(aFilters);
         }
     });
 });
